50fb352faae3e4300e0aa796c361ea6bd1d8cda6
Aaradhya <aaradhyachachra779@gmail.com>
2026-09-07 20:03:48 +0530

    Add Monaco Editor and xterm Terminal

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Editor from "@monaco-editor/react";
+import TerminalPanel from "./components/Terminal";
 
 function App() {
   return (
@@ -6,12 +8,23 @@ function App() {
       <div className="top">
         <div className="explorer">Explorer</div>
 
-        <div className="editor">Editor</div>
+        <div className="editor">
+         <Editor
+         height="100%"
+        defaultLanguage="typescript"
+        defaultValue={`function hello() {
+        console.log("Hello Personal AI IDE");
+         }`}
+            theme="vs-dark"
+             />
+        </div>
 
         <div className="agent">AI Agent</div>
       </div>
 
-      <div className="terminal">Terminal</div>
+      <div className="terminal">
+              <TerminalPanel />
+        </div>
     </div>
   );
 }
---
+++ desktop/src/components/Terminal.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef } from "react";
+import { Terminal } from "@xterm/xterm";
+import { FitAddon } from "@xterm/addon-fit";
+
+import "@xterm/xterm/css/xterm.css";
+
+export default function TerminalPanel() {
+  const terminalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!terminalRef.current) return;
+
+    const term = new Terminal({
+      cursorBlink: true,
+      theme: {
+        background: "#1e1e1e",
+      },
+    });
+
+    const fitAddon = new FitAddon();
+
+    term.loadAddon(fitAddon);
+
+    term.open(terminalRef.current);
+
+    setTimeout(() => {
+      fitAddon.fit();
+    }, 100);
+
+    term.writeln("Personal AI IDE");
+    term.writeln("");
+    term.writeln("Terminal Ready...");
+
+    const resizeHandler = () => {
+      fitAddon.fit();
+    };
+
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+      term.dispose();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={terminalRef}
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  );
+}
