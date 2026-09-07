d073f994e07159a4bde2fc4cf9b56ec628c76f5e
Aaradhya <aaradhyachachra779@gmail.com>
2026-09-07 19:26:32 +0530

    Initial project structure and React IDE shell

---
+++ desktop/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="top">
+        <div className="explorer">Explorer</div>
+
+        <div className="editor">Editor</div>
+
+        <div className="agent">AI Agent</div>
+      </div>
+
+      <div className="terminal">Terminal</div>
+    </div>
+  );
+}
+
+export default App;
